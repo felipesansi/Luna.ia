fb882cbb1b2dd5ac1fa53706ce98b70a165a7ddb
Felipe Freitas <felipeprofissional22@gmail.com>
2024-10-11 15:49:46 -0300

    luna v2

--- luna.py
+++ luna.py
@@ -8,18 +8,18 @@ import time
 import webbrowser
 import pywhatkit as kit
 import pyowm
+import threading
 
 # Inicializa o reconhecedor de voz e o mixer do pygame
 audio = sr.Recognizer()
 pygame.mixer.init()
 
-
-owm= pyowm.OWM('166936b11d98f4fc3e044f70dd6fa985')
+# Substitua pela sua chave de API do OpenWeatherMap
+owm = pyowm.OWM('166936b11d98f4fc3e044f70dd6fa985')
 mgr = owm.weather_manager()
 silencio = False
 
 def Falar(texto):
-  #  Converte texto em fala usando gTTS e reproduz o áudio gerado.
     global silencio
     if silencio:
         return
@@ -34,112 +34,122 @@ def Falar(texto):
     
     # Aguardar até a reprodução terminar
     while pygame.mixer.music.get_busy():
-        time.sleep(0.1)
+        time.sleep(0.05)  # Reduz o tempo de espera ativo
 
-    # Remove o arquivo após a reprodução
-   # os.remove(arquivo_audio)
+    # Certificar-se de que a música não está mais sendo reproduzida antes de remover
+    pygame.mixer.music.unload()
 
-def executa_comando(source):
+    # Verificar se o arquivo existe antes de tentar removê-lo
+    if os.path.exists(arquivo_audio):
+        try:
+            os.remove(arquivo_audio)
+        except PermissionError:
+            print(f"Erro ao tentar remover o arquivo {arquivo_audio}. Arquivo está em uso.")
+        except Exception as e:
+            print(f"Ocorreu um erro ao tentar remover o arquivo {arquivo_audio}: {e}")
+
+def reconhecer_comando():
     """Reconhece o comando de voz do usuário."""
-    try:
-        audio.adjust_for_ambient_noise(source)  # Ajusta para o ruído ambiente
-        Falar("Estou ouvindo")
-        voz = audio.listen(source)
-        comando = audio.recognize_google(voz, language='pt-BR')
-        return comando.lower()  # Retorna o comando em minúsculas
-    except sr.UnknownValueError:
-        print("Não consegui entender o áudio.")
-        Falar("Desculpe, não consegui entender o que você disse.")
-        return None
-    except sr.RequestError:
-        print("Erro ao se conectar ao serviço de reconhecimento de voz.")
-        Falar("Houve um problema com o serviço de reconhecimento de voz.")
-        return None
-    except Exception as e:
-        print("Ocorreu um erro:", e)
-        Falar("Ocorreu um erro inesperado.")
-        return None
+    with sr.Microphone() as source:
+        try:
+            audio.adjust_for_ambient_noise(source)  # Ajusta para o ruído ambiente
+            Falar("Estou ouvindo")
+            voz = audio.listen(source)
+            comando = audio.recognize_google(voz, language='pt-BR')
+            return comando.lower()  # Retorna o comando em minúsculas
+        except sr.UnknownValueError:
+            print("Não consegui entender o áudio.")
+            Falar("Desculpe, não consegui entender o que você disse.")
+            return None
+        except sr.RequestError:
+            print("Erro ao se conectar ao serviço de reconhecimento de voz.")
+            Falar("Houve um problema com o serviço de reconhecimento de voz.")
+            return None
+        except Exception as e:
+            print("Ocorreu um erro:", e)
+            Falar("Ocorreu um erro inesperado.")
+            return None
+
+def traduzir_clima(descricao):
+    """Traduz a descrição do clima do inglês para o português."""
+    traducoes = {
+        "clear sky": "céu limpo",
+        "few clouds": "poucas nuvens",
+        "scattered clouds": "nuvens dispersas",
+        "broken clouds": "nuvens quebradas",
+        "overcast clouds": "nublado",
+        "shower rain": "chuva de banho",
+        "rain": "chuva",
+        "thunderstorm": "trovoada",
+        "snow": "neve",
+        "mist": "névoa"
+    }
+    return traducoes.get(descricao, descricao)
+
+def executar_comando(comando):
+    """Executa o comando de voz reconhecido."""
+    global silencio
+    if 'o que você pode fazer' in comando:
+        Falar('Eu posso falar as horas e fazer pesquisas na Wikipédia.')
+    elif 'horas são' in comando:
+        horas = datetime.datetime.now().strftime('%H:%M')
+        Falar("Agora são " + horas)
+    elif 'me diga sobre' in comando or 'me fale sobre' in comando:
+        if 'me diga sobre' in comando:
+            sobre = comando.replace('me diga sobre', '').strip()
+        else:
+            sobre = comando.replace('me fale sobre', '').strip()
+        wikipedia.set_lang('pt')
+        try:
+            Falar("Procurando sobre " + sobre)
+            resposta = wikipedia.summary(sobre, sentences=2)
+            Falar(resposta)
+        except wikipedia.exceptions.DisambiguationError:
+            Falar("A pesquisa retornou múltiplas opções. Por favor, seja mais específico.")
+    elif 'youtube' in comando:
+        Falar('Sim, senhor. Abrindo YouTube agora...')
+        webbrowser.open('https://www.youtube.com')
+    elif 'toque' in comando:
+        toque = comando.replace('toque', '').strip()
+        Falar('Tocando ' + toque)
+        threading.Thread(target=kit.playonyt, args=(toque,)).start()  # Executa em uma nova thread
+    elif 'qual a temperatura em' in comando:
+        cidade = comando.replace('qual a temperatura em', '').strip()
+        Falar('Obtendo informações do clima em ' + cidade)
+        try:
+            observacao = mgr.weather_at_place(cidade)
+            clima = observacao.weather
+            temperatura = clima.temperature('celsius')['temp']
+            descricao = traduzir_clima(clima.detailed_status)
+            Falar(f'Em {cidade} a temperatura é {temperatura:.1f}°C e o tempo está {descricao}.')
+        except pyowm.commons.exceptions.NotFoundError:
+            Falar(f"Não consegui encontrar informações climáticas para {cidade}.")
+    elif 'procure por' in comando:
+        pesquisa = comando.replace('procure por', '').strip()
+        Falar('Procurando no Google por ' + pesquisa)
+        threading.Thread(target=kit.search, args=(pesquisa,)).start()  # Executa em uma nova thread
+    elif 'sair' in comando or 'encerrar' in comando:
+        Falar("Encerrando o assistente. Até logo!")
+        exit()
+    elif 'pare de falar' in comando or 'silêncio' in comando:
+        silencio = True
+        Falar("Entrando em modo silencioso.")
+ 
+    elif 'luna' in comando or 'fim do silêncio' in comando:
+        silencio = False
+        Falar("Saindo do modo silencioso.")
+    else:
+        Falar("Você não me chamou pelo nome.")
 
 def comando_voz_usuario():
     """Processa os comandos de voz do usuário."""
-    global silencio
-    with sr.Microphone() as source:
-        while True:
-            comando = executa_comando(source)
-            if comando:
-                if 'luna' in comando:  
-                    comando = comando.replace('luna', '').strip()
-                    
-                    if 'o que você pode fazer' in comando:
-                        Falar('Eu posso falar as horas e fazer pesquisas na Wikipédia.')
-                    
-                    elif 'horas são' in comando:
-                        horas = datetime.datetime.now().strftime('%H:%M')
-                        Falar("Agora são " + horas)
-                        silencio = True               
-                    elif 'me diga sobre' in comando or 'me fale sobre' in comando:
-                        if 'me diga sobre' in comando:
-                            sobre = comando.replace('me diga sobre', '').strip()
-                        else:
-                            sobre = comando.replace('me fale sobre', '').strip()
-
-                        wikipedia.set_lang('pt')
-                        try:
-                            Falar("Procurando sobre " + sobre)
-                            resposta = wikipedia.summary(sobre, sentences=2)
-                            Falar(resposta)
-                            silencio = True 
-                        
-                        except wikipedia.exceptions.DisambiguationError:
-                            Falar("A pesquisa retornou múltiplas opções. Por favor, seja mais específico.")
-                    
-                    elif 'youtube' in comando:
-                        Falar('Sim, senhor. Abrindo YouTube agora...')
-                        webbrowser.open('https://www.youtube.com')
-                        silencio = True 
-                        
-                    elif 'toque' in comando:
-                        toque = comando.replace('toque', '').strip()
-                        Falar('Tocando ' + toque)
-                        kit.playonyt(toque)
-                        silencio = True 
-
-                    elif 'qual a temperatura em' in comando:
-                        cidade = comando.replace('qual a temperatura em', '').strip()
-                        Falar('Obtendo informções do clima em ' + cidade)
-                       
-                        try:
-                         
-                           observacao = mgr.weather_at_place(cidade)
-                           clima = observacao.weather
-                           temperatura = clima.temperature('celsius')['temp']
-                           descricao = clima.detailed_status
-                           Falar(f'Em {cidade} a temperatura é {temperatura:.1f}°C e o tempo está {descricao}.')
-                           silencio = True
-                       
-                        except pyowm.commons.exceptions.NotFoundError:
-                          Falar(f"Não consegui encontrar informações climáticas para {cidade}.")
-                    
-                    elif 'procure por' in comando:
-                        pesquisa = comando.replace('procure por', '').strip()
-                        Falar('Procurando no Google por ' + pesquisa)
-                        kit.search(pesquisa)
-                        silencio = True 
-                    
-                    elif 'sair' in comando or 'encerrar' in comando:
-                        Falar("Encerrando o assistente. Até logo!")
-                        break  
-                    
-                    elif 'pare de falar' in comando or 'silêncio' in comando:
-                        silencio = True
-                        Falar("Entrando em modo silencioso.")
-                    
-                    elif 'ei, luna' in comando or 'fim do silêncio' in comando:
-                        silencio = False
-                        Falar("Saindo do modo silencioso.")
-                    
-                else:
-                    Falar("Você não chamou me pelo nome.")
+    while True:
+        comando = reconhecer_comando()
+        if comando and 'luna' in comando:
+            comando = comando.replace('luna', '').strip()
+            executar_comando(comando)
+        else:
+            Falar("Você não me chamou pelo nome.")
 
 if __name__ == "__main__":
     comando_voz_usuario()
